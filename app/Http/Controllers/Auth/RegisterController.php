8f7144d596015e914819e7ffdeb20665597fd684
ArKaNeMaN <arkaneman1@gmail.com>
2023-02-26 22:52:35 +0300

    Move database to storage

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Enums\UserRole;
-use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
