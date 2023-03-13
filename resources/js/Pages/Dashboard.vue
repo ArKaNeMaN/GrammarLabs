4f1908723390e20e5db74ba70dcd6a410c2d3333
ArKaNeMaN <arkaneman1@gmail.com>
2023-03-14 00:02:58 +0300

    Add scroll for tables in dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -49,63 +49,67 @@ function sendAnswer(answer) {
     <authenticated-layout header="Главная страница">
         <card-block class="mt-4 space-y-2">
             <card-header>Список активных заданий</card-header>
-            <table-layout class="w-full">
-                <table-simple-header :ths="[
-                    'Задание',
-                    'Дата выдачи',
-                    'Статус',
-                    'Действия',
-                ]"/>
-                <tbody v-if="!isEmpty(assignedTasks)">
-                <tr v-for="assignedTask in assignedTasks">
-                    <td>{{ assignedTask.task.name }}</td>
-                    <td>{{ dateFormat(assignedTask.created_at) }}</td>
-                    <td>{{ taskStatusFormat(assignedTask.lastAnswer?.status ?? null) }}</td>
-                    <td class="flex space-x-2">
-                        <ln :href="route('tasks.answers.new.show', assignedTask.id)">
-                            <secondary-button>Новое решение</secondary-button>
-                        </ln>
-                    </td>
-                </tr>
-                </tbody>
-                <table-text-row
-                    v-else
-                    class="text-center text-gray-500"
-                >Нет назначенных заданий</table-text-row>
-            </table-layout>
+            <div class="overflow-auto">
+                <table-layout class="w-full">
+                    <table-simple-header :ths="[
+                        'Задание',
+                        'Дата выдачи',
+                        'Статус',
+                        'Действия',
+                    ]"/>
+                    <tbody v-if="!isEmpty(assignedTasks)">
+                    <tr v-for="assignedTask in assignedTasks">
+                        <td>{{ assignedTask.task.name }}</td>
+                        <td>{{ dateFormat(assignedTask.created_at) }}</td>
+                        <td>{{ taskStatusFormat(assignedTask.lastAnswer?.status ?? null) }}</td>
+                        <td class="flex space-x-2">
+                            <ln :href="route('tasks.answers.new.show', assignedTask.id)">
+                                <secondary-button>Новое решение</secondary-button>
+                            </ln>
+                        </td>
+                    </tr>
+                    </tbody>
+                    <table-text-row
+                        v-else
+                        class="text-center text-gray-500"
+                    >Нет назначенных заданий</table-text-row>
+                </table-layout>
+            </div>
         </card-block>
         <card-block class="mt-2 space-y-2">
             <card-header>Сохранённые решения</card-header>
-            <table-layout class="w-full">
-                <table-simple-header :ths="[
-                    '№',
-                    'Задание',
-                    'Дата обновления',
-                    'Статус',
-                    'Действия',
-                ]"/>
-                <tbody v-if="!isEmpty(answers)">
-                <tr v-for="answer in answers">
-                    <td>{{ answer.id }}</td>
-                    <td>{{ answer.assigned_task.task.name }}</td>
-                    <td>{{ dateFormat(answer.updated_at) }}</td>
-                    <td>{{ taskStatusFormat(answer.status) }}</td>
-                    <td class="flex space-x-2">
-                        <ln :href="route('tasks.answers.edit.show', [answer.assigned_task.id, answer.id])">
-                            <secondary-button>Перейти</secondary-button>
-                        </ln>
-                        <primary-button
-                            v-if="answer.status === 'draft'"
-                            @click="sendAnswer(answer)"
-                        >Сдать</primary-button>
-                    </td>
-                </tr>
-                </tbody>
-                <table-text-row
-                    v-else
-                    class="text-center text-gray-500"
-                >Нет сохранённых решений</table-text-row>
-            </table-layout>
+            <div class="overflow-auto">
+                <table-layout class="w-full">
+                    <table-simple-header :ths="[
+                        '№',
+                        'Задание',
+                        'Дата обновления',
+                        'Статус',
+                        'Действия',
+                    ]"/>
+                    <tbody v-if="!isEmpty(answers)">
+                    <tr v-for="answer in answers">
+                        <td>{{ answer.id }}</td>
+                        <td>{{ answer.assigned_task.task.name }}</td>
+                        <td>{{ dateFormat(answer.updated_at) }}</td>
+                        <td>{{ taskStatusFormat(answer.status) }}</td>
+                        <td class="flex space-x-2">
+                            <ln :href="route('tasks.answers.edit.show', [answer.assigned_task.id, answer.id])">
+                                <secondary-button>Перейти</secondary-button>
+                            </ln>
+                            <primary-button
+                                v-if="answer.status === 'draft'"
+                                @click="sendAnswer(answer)"
+                            >Сдать</primary-button>
+                        </td>
+                    </tr>
+                    </tbody>
+                    <table-text-row
+                        v-else
+                        class="text-center text-gray-500"
+                    >Нет сохранённых решений</table-text-row>
+                </table-layout>
+            </div>
         </card-block>
     </authenticated-layout>
 </template>
